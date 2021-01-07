f401bd6bd6018ffebefd2c0cd30cc0b1aa865ab0
jrwyman <jacobwyman1995@gmail.com>
2021-01-07 15:24:24 -0700

    refactor comments and finished removing containers

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 
 const Post = require('../models/Post');
 const validatePostInput = require('../validation/posts');
+const validateCommentInput = require('../validation/comment');
 
 router.patch('/:id/like',
   passport.authenticate('jwt', { session: false }),
@@ -27,32 +28,41 @@ router.patch('/:id/like',
 
 router.patch('/:id/comment',
   passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Post.findById(req.params.id)
-      .then((post) => {
+  async (req, res) => {
+    console.log(req);
+      const error = validateCommentInput(req.body.text)
+
+      if (error) {
+        return res.status(404).json(error)
+      }
+
+      try {
+        const post = await Post.findById(req.params.id)
+        
         post.comments.push({
           user_id: req.user.id,
           username: req.user.username,
           text: req.body.text,
         });
-        post.save();
-        res.json(post);
-      })
-      .catch((err) => res.status(404).json({ error: err }));
+        await post.save();
+        return res.json(post.comments);
+      } catch (e) {
+        return res.status(400).json({error: "comments had an issue"})
+      }
   });
 
 router.get('/', (req, res) => {
   Post.find()
     .sort({ createdAt: 'desc' })
     .then((posts) => res.json(posts))
-    .catch((err) => res.status(404).json({ error: err }));
+    .catch((err) => res.status(400).json({ error: err }));
 });
 
 router.get('/user/:user_id', (req, res) => {
   Post.find({ 'user.id': req.params.user_id })
     .sort({ createdAt: 'desc' })
     .then((posts) => res.json(posts))
-    .catch((err) => res.status(404).json({ error: err }));
+    .catch((err) => res.status(400).json({ error: err }));
 });
 
 router.post('/',
@@ -79,7 +89,7 @@ router.delete('/:id',
     Post.findOneAndDelete({ _id: req.params.id })
       .then((post) => {
         if (post === null) {
-          res.status(400).json('No post found');
+          res.status(404).json('No post found');
         } else {
           res.json('Post deleted!');
         }
